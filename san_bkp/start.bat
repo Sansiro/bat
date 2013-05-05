fef38014a4d40a6de400672a60f081857ee448c8
Sergii Pidlypenskyi <sansiro.gis@gmail.com>
2013-05-06 02:45:17 +0300

    parsing option "auto" for create backup in certain 4 days separated by comma check for existing config files

--- san_bkp/start.bat
+++ san_bkp/start.bat
@@ -1,9 +1,5 @@
 @echo off
 
-echo Thank for color text code http://stackoverflow.com/users/1012053/dbenham
-
-
-
 :: TODO
 
 :: Find executies and settings
@@ -19,43 +15,68 @@ rem example: call :color 0c,0a,0b,0d "text"
 
 call :initColorPrint
 
-
+call :color 07 "Thank for colored text code http://stackoverflow.com/users/1012053/dbenham"
 echo.
 echo.
 call :color 9f " * San Backup Free v0.1 May'2013" /n
 echo.
 
 rem Init the program
-if not exist "%~dp0.\config\settings.lst" (
+
+set settings=%~dp0.\config\settings.lst
+echo %settings%
+if not exist %settings% (
 		call :color 0c " * Settings file wasn't found. Reistall the program."
 		echo.
 		pause > nul
 		exit
-) else (
-		::set settings=.\config\settings.lst
-		set settings="%~dp0.\config\settings.lst"
 )
 
-if not exist "%~dp0.\config\tasks.lst" (
+set tasks=%~dp0.\config\tasks.lst
+echo %tasks%
+if not exist %tasks% (
 		call :color 0c " * You need to have any tasks in the list. "
 		call :color 0e "Please, look readme.txt"
 		echo.
 		pause > nul
 		exit
-) else (
-		::set settings=.\config\settings.lst
-		set settings="%~dp0.\config\settings.lst"
+)
+
+set exclus=%~dp0.\config\exclusions.lst3
+echo %exclus%
+if not exist %exclus% (
+		call :color 0c " * Exclusion file missing or unreachable."
+		call :color 0e "Trying to create new file exclusions.lst"
+		echo.
+		echo .lck >> "%exclus%"
+)
+
+if not exist %exclus% (
+		call :color 0c " * Can't create exclusions.lst"
+		pause > nul
+		exit
 )
 
 rem Parsing the settings
 rem format: key=value; some description
+
 for /F "usebackq eol=; tokens=1,2 delims==" %%a in (%settings%) do (
 		if %%a==arc set arc=%%b
-)
+		if %%a==days (
+			set days=%%b
+			call :parseDays
+			);
+);
+		echo arc=!arc!
+		echo day1=!day1!
+		echo day2=!day2!
+		echo day3=!day3!	
+		echo day4=!day4!
 
 rem Parsing the tasks
 rem format: "source"<tab>"destination"<tab>"daily|full|auto"<tab>"zip|7z|rar|no"<tab>"0-9"
-for /F "usebackq skip=2 eol=; tokens=1-5 delims=	" %%a in (.\config\tasks.lst) do (
+
+for /F "usebackq skip=2 eol=; tokens=1-5 delims=	" %%a in (%tasks%) do (
 
 		set V1=%%a
 		set V2=%%b
@@ -83,14 +104,15 @@ for /F "usebackq skip=2 eol=; tokens=1-5 delims=	" %%a in (.\config\tasks.lst) d
 				echo * Copying to SYSTEMP
 				);
 		);
-		
+
+
 		if !V3!=="daily" (
 			set method=daily
-			xcopy /D /E /H /I /Y /EXCLUDE:.\config\exclusions.lst !V1! !V2!
+			xcopy /D /E /H /I /Y /EXCLUDE:!exclus! !V1! !V2!
 			);
 		if !V3!=="full" (
 			set method=full
-			xcopy /E /H /I /Y /EXCLUDE:.\config\exclusions.lst !V1! !V2!
+			xcopy /E /H /I /Y /EXCLUDE:!exclus! !V1! !V2!
 			); 
 		if !V3!=="auto" (
 			set method=auto
@@ -101,10 +123,21 @@ for /F "usebackq skip=2 eol=; tokens=1-5 delims=	" %%a in (.\config\tasks.lst) d
 		
 )
 
+
+
+call :color 0a " * Everything was OK"
+pause > nul
+
 call :cleanupColorPrint
 
-echo * Everything was OK
-pause > nul
+:parseDays
+for /F "tokens=1-4 delims=," %%a in ("%days%") do (
+		set day1=%%a
+		set day2=%%b
+		set day3=%%c
+		set day4=%%d			
+)
+exit /b
 
 :color Color  Str  [/n]
 setlocal
@@ -131,6 +164,5 @@ exit /b
 :cleanupColorPrint
 del "%temp%\x"
 exit /b
-
 
 
